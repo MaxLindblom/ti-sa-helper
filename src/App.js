391eb5dcb8db608864d264bd979afa0b9d72c700
Max Lindblom <max.g.lindblom@gmail.com>
2022-01-25 10:42:06 +0100

    Header background fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { React } from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Strategy from './routes/Strategy';
 import Races from './routes/Races';
 import Home from './routes/Home';
@@ -12,16 +12,17 @@ function App() {
   document.body.style.background = '#282c34';
   return (
     <>
-      <Header />
-      <Navbar className="navbar-container" bg="primary">
-        <Nav>
-          <Nav.Link href="home">Home</Nav.Link>
-          <Nav.Link href="strategy">Strategy Cards</Nav.Link>
-          <Nav.Link href="races">Race Sheets</Nav.Link>
-        </Nav>
-      </Navbar>
       <div className="App">
+        <Header />
+        <Navbar className="navbar-container" bg="primary">
+          <Nav>
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="strategy">Strategy Cards</Nav.Link>
+            <Nav.Link href="races">Race Sheets</Nav.Link>
+          </Nav>
+        </Navbar>
         <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/strategy" element={<Strategy />} />
           <Route path="/races" element={<Races />} />
