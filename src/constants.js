8a9440a7cbedee094c118336db641925b2a23478
Max Lindblom <max.g.lindblom@gmail.com>
2022-01-25 11:23:36 +0100

    Extract constants into separate file

---
+++ src/constants.js
@@ -0,0 +1,47 @@
+const races = [
+  'Arborec',
+  'Creuss',
+  'Hacan',
+  'Jol-Nar',
+  'Krotoan',
+  'Letnev',
+  'Lizix',
+  'Mentak',
+  'Muaat',
+  'Naalu',
+  'Saar',
+  'Sardakk',
+  'Sol',
+  'Winnu',
+  'Xxcha',
+  'Yin',
+  'Yssaril',
+];
+
+const cardImages = [
+  '../../strategy-cards/xLeadership.jpg',
+  '../../strategy-cards/xDiplomacy.jpg',
+  '../../strategy-cards/xAssemblyTC.jpg',
+  '../../strategy-cards/xProduction.jpg',
+  '../../strategy-cards/xTrade.jpg',
+  '../../strategy-cards/xWarfare.jpg',
+  '../../strategy-cards/xTechnology.jpg',
+  '../../strategy-cards/xBureaucracy.jpg',
+  '../../strategy-cards/xProspect.jpg',
+];
+
+const unitTypes = [
+  'War sun',
+  'Dreadnought',
+  'Cruiser',
+  'Destroyer',
+  'Carrier',
+  'Fighter',
+  'Flagship',
+  'PDS',
+  'Mech',
+  'Ground force',
+  'Shock troop',
+];
+
+export { races, cardImages, unitTypes };
